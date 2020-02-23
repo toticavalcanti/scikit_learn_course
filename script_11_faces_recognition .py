d1bcf1a78291e0943bb5eb71715f6ff32e5d4f6c
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-02-23 09:17:37 -0300

    update comments in script 11

--- script_11_faces_recognition .py
+++ script_11_faces_recognition .py
@@ -126,7 +126,7 @@ print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
 # Avaliação qualitativa das previsões usando matplotlib
 
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-    """Helper function para plotar a galeria de fotos"""
+    """Função Helper para plotar a galeria de fotos"""
     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
     plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
     for i in range(n_row * n_col):
